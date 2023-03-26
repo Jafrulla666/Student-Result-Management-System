496422283e498384178b504249217386b2dd14f4
Jafrulla666 <59016429+Jafrulla666@users.noreply.github.com>
2023-03-26 15:59:41 +0530

    back restored

--- aboutus.php
+++ aboutus.php
@@ -1,151 +1,81 @@
-
-
-//To Handle Session Variables on This Page
-//session_start();
-
-
-//Including Database Connection From db.php file to avoid rewriting in all files
-//require_once("db.php");
+<?php include('header.php');?>
 
 <!DOCTYPE html>
-
+<html>
 <head>
-
-    <title>Placement Portal</title>
-
-
-    <?php
-
-    include 'php/head.php'
-
-    ?>
-
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/aboutus.css">
 </head>
-
-
 <body>
 
-    <?php
-    include 'php/header.php'
+<div class="about-section">
+  <h1>PANCHNAMA</h1>
+  <p>We are a team of 5 members colaborating in a project and made all possible efforts over nights to make it hope you like it.</p>
+  <p> hope this may give us a good marks.</p>
+</div>
 
-    ?>
+<h2 style="text-align:center">Our Team</h2>
+<div class="row">
+     <div class="column">
+          <div class="card">
+               <img src="img/mohak.png" alt="Mayank Mohak" style="width:100%">
+               <div class="container">
+                    <h2>MAYANK MOHAK</h2>
+                    <p class="title">TEAM:- PANCHNAMA</p>
+                    <p>Ph No.   :   +91 - 9525177622</p>
+                    <p>mayank8199@gmail.com</p>
+                    <p><a href="https://forms.gle/dJRt4DuWtqh417n19"> <button class="button">Contact</button></a></p>
+               </div>
+          </div>
+     </div>
 
-    <!-- ======= F.A.Q Section ======= -->
-
-    <section id="faq" class="faq">
-        <div class="container-fluid" data-aos="fade-up">
-
-            <div class="row gy-4">
-
-                <div class="col-lg-7 d-flex flex-column justify-content-center align-items-stretch  order-2 order-lg-1">
-
-                    <div class="content px-xl-5">
-                        <h3>Frequently Asked <strong>Questions</strong></h3>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                            dolore magna aliqua. Duis aute irure dolor in reprehenderit
-                        </p>
-                    </div>
-
-                    <div class="accordion accordion-flush px-xl-5" id="faqlist">
-
-                        <div class="accordion-item" data-aos="fade-up" data-aos-delay="200">
-                            <h3 class="accordion-header">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-1">
-                                    <i class="bi bi-question-circle question-icon"></i>
-                                    Non consectetur a erat nam at lectus urna duis?
-                                </button>
-                            </h3>
-                            <div id="faq-content-1" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                                <div class="accordion-body">
-                                    Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id volutpat lacus laoreet non curabitur
-                                    gravida. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus non.
-                                </div>
-                            </div>
-                        </div><!-- # Faq item-->
-
-                        <div class="accordion-item" data-aos="fade-up" data-aos-delay="300">
-                            <h3 class="accordion-header">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-2">
-                                    <i class="bi bi-question-circle question-icon"></i>
-                                    Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque?
-                                </button>
-                            </h3>
-                            <div id="faq-content-2" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                                <div class="accordion-body">
-                                    Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id
-                                    donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque
-                                    elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-                                </div>
-                            </div>
-                        </div><!-- # Faq item-->
-
-                        <div class="accordion-item" data-aos="fade-up" data-aos-delay="400">
-                            <h3 class="accordion-header">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-3">
-                                    <i class="bi bi-question-circle question-icon"></i>
-                                    Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi?
-                                </button>
-                            </h3>
-                            <div id="faq-content-3" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                                <div class="accordion-body">
-                                    Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar
-                                    elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque
-                                    eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis
-                                    sed odio morbi quis
-                                </div>
-                            </div>
-                        </div><!-- # Faq item-->
-
-                        <div class="accordion-item" data-aos="fade-up" data-aos-delay="500">
-                            <h3 class="accordion-header">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-4">
-                                    <i class="bi bi-question-circle question-icon"></i>
-                                    Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?
-                                </button>
-                            </h3>
-                            <div id="faq-content-4" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                                <div class="accordion-body">
-                                    <i class="bi bi-question-circle question-icon"></i>
-                                    Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id
-                                    donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque
-                                    elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
-                                </div>
-                            </div>
-                        </div><!-- # Faq item-->
-
-                        <div class="accordion-item" data-aos="fade-up" data-aos-delay="600">
-                            <h3 class="accordion-header">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#faq-content-5">
-                                    <i class="bi bi-question-circle question-icon"></i>
-                                    Tempus quam pellentesque nec nam aliquam sem et tortor consequat?
-                                </button>
-                            </h3>
-                            <div id="faq-content-5" class="accordion-collapse collapse" data-bs-parent="#faqlist">
-                                <div class="accordion-body">
-                                    Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in.
-                                    Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est.
-                                    Purus gravida quis blandit turpis cursus in
-                                </div>
-                            </div>
-                        </div><!-- # Faq item-->
-
-                    </div>
-
-                </div>
-
-                <div class="col-lg-5 align-items-stretch order-1 order-lg-2 img" style='background-image: url("assets/img/faq.jpg");'>&nbsp;</div>
-            </div>
-
-        </div>
-    </section><!-- End F.A.Q Section -->
-
-    <?php
-
-    include 'php/footer.php'
-
-    ?>
-
+     <div class="column">
+          <div class="card">
+               <img src="img/gupta.png" alt="Mayank Gupta" style="width:100%">
+               <div class="container">
+                    <h2>Mayank Gupta</h2>
+                    <p class="title">TEAM:- PANCHNAMA</p>
+                    <p>Ph No.   :   +91 - 7735953025</p>
+                    <p>minku2711@example.com</p>
+                    <p><a href="https://forms.gle/dJRt4DuWtqh417n19"> <button class="button">Contact</button></a></p>
+               </div>
+          </div>
+     </div>
+     <div class="column">
+          <div class="card">
+               <img src="img/issak.png" alt="Issak Nayank" style="width:100%">
+               <div class="container">
+                    <h2>Issak Nayank</h2>
+                    <p class="title">TEAM:- PANCHNAMA</p>
+                    <p>Ph No.   :   +91 - 9556601349</p>
+                    <p>issak @gamil.com</p>
+                    <p><a href="https://forms.gle/dJRt4DuWtqh417n19"> <button class="button">Contact</button></a></p>
+               </div>
+          </div>
+     </div>
+     <div class="column">
+          <div class="card">
+               <img src="img/adarsh.png" alt="Adarsh Gupta" style="width:100%">
+               <div class="container">
+                    <h2>Adarsh Gupta</h2>
+                    <p class="title">TEAM:- PANCHNAMA</p>
+                    <p>Ph No.   :   +91 - 8327775034</p>
+                    <p>adarsh@gmail.com</p>
+                    <p><a href="https://forms.gle/dJRt4DuWtqh417n19"> <button class="button">Contact</button></a></p>
+               </div>
+          </div>
+     </div>
+     <div class="column">
+          <div class="card">
+               <img src="img/faizan.jpg" alt="Faizan Ansari" style="width:100%">
+               <div class="container">
+                    <h2>Faizan Ansari</h2>
+                    <p class="title">TEAM:- PANCHNAMA</p>
+                    <p>Ph No.   :   +91 - 747855263</p>
+                    <p>faizanansari@gmail.com</p>
+                    <p><a href="https://forms.gle/dJRt4DuWtqh417n19"> <button class="button">Contact</button></a></p>
+               </div>
+          </div>
+     </div>
 </body>
-
 </html>
